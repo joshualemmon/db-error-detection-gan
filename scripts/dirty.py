b6739a9c9331ea4d678bd426e9f99a4947ff8b5d
joshualemmon <joshua.lemmon@uoit.net>
2020-03-01 11:58:21 -0500

    Created scripts to convert generated xml to csv, and to dirty a percentange of a csv randomly

--- scripts/dirty.py
+++ scripts/dirty.py
@@ -1,13 +1,80 @@
 import numpy as np
 import argparse
+from string import ascii_letters
+import csv
+import random
+
+ascii_letters = ascii_letters[:26]
+
+def make_dirty(vals):
+	dirty = []
+	for v in vals:
+		ind = int(np.random.randint(0, len(v)-1))
+		if v[ind].isnumeric():
+			v[ind] = dirty_numeric(v[ind])
+		else:
+			v[ind] = dirty_categorical(v[ind])
+		v[-1] = 1
+		dirty.append(v)
+	return dirty
+
+# Randomly negate or scale number by factor of 10
+def dirty_numeric(val):
+	i = np.random.randint(0,3)
+	if i == 0:
+		return val*-1
+	elif val == 1:
+		return val*10
+	elif val == 2:
+		return int(val/10)
+
+# Randomly change character, swap characters or insert character
+def dirty_categorical(val):
+	i = np.random.randint(0,4)
+	val = list(val)
+	#change random character in string to random letter
+	if i == 0:
+		ind = np.random.randint(1,len(val))
+		val[ind] = random.choice(ascii_letters)
+	# Randomly swap two adjacent characters
+	if i == 1:
+		ind = np.random.randint(1, len(val)-1)
+		c = val[ind]
+		val[ind] = val[ind+1]
+		val[ind+1] = c
+	# Insert random letter into string
+	if i == 2:
+		ind = np.random.randint(1, len(val))
+		val = val[:ind] + [random.choice(ascii_letters)] + val[ind:]
+	# Randomly delete character
+	if i == 3:
+		ind = np.random.randint(1, len(val))
+		val = val[:ind] + val[ind:]
+	val = "".join(val)
+	return val
 
 def main(args):
 	infile = args.infile
 	out = args.out
+	perc = args.percent
+
+	data = [l.rstrip('\n').split(',') for l in open(infile, 'r').readlines()]
+	split_index = int(len(data)*(perc/100))
+	clean = data[:-split_index]
+	dirty = data[-split_index:]
+	# print(len(clean), len(dirty))
+	dirty = make_dirty(dirty)
+
+	with open(out, 'w') as f:
+		writer = csv.writer(f, delimiter=',')
+		# f_name, l_name, email, age, salary, height, interest, country, clean/dirty
+		writer.writerows(clean)
+		writer.writerows(dirty)
 
 
 if __name__ == "__main__":
 	ap = argparse.ArgumentParser()
 	ap.add_argument('--infile', '-i', type=str, required=True)
 	ap.add_argument('--out', '-o', type=str, required=True)
+	ap.add_argument('--percent', '-p', type=int, required=True)
 	main(ap.parse_args())
--- scripts/xml_to_csv.py
+++ scripts/xml_to_csv.py
@@ -10,12 +10,14 @@ def get_xml_vals(infile):
 	root = tree.getroot()
 	item = ""
 	attr = []
+	# Get attribute names
 	for r in root:
 		item = r.tag
 		for c in r:
 			attr.append(c.tag)
 	attr = set(attr)
 
+	# Get attribute values for each item
 	vals = []
 	for p in list(root):
 		v = []
@@ -27,14 +29,17 @@ def get_xml_vals(infile):
 
 def add_attributes(xml_data):
 	data = []
+	# Load possible interests and countries
 	interests = [i.rstrip('\n') for i in open('interests.txt', 'r').readlines()]
 	countries = [c.rstrip('\n') for c in open('countries.txt', 'r').readlines()]
+	# Generate random valid values for each tuple
 	for x in xml_data:
 		x.append(gen_age())
 		x.append(gen_salary())
 		x.append(gen_height())
 		x.append(gen_interest(interests))
 		x.append(gen_country(countries))
+		x.append(0)
 		data.append(x)
 	return data
 
@@ -75,14 +80,14 @@ def main(args):
 	infile = args.infile
 	out = args.out
 
+	# Read XML data
 	xml_data = get_xml_vals(infile)
 
 	full_data = add_attributes(xml_data)
 
 	with open(out, 'w') as f:
 		writer = csv.writer(f, delimiter=',')
-		writer.writerow()
-		# f_name, l_name, email, age, salary, height, interest, country
+		# f_name, l_name, email, age, salary, height, interest, country, clean/dirty
 		writer.writerows(full_data)
 
 if __name__ == '__main__':
